c42cd754479ca2fe02661ce2107b13ade45165d7
Dmitry Kozlov <dmitry.f.kozlov@gmail.com>
2022-02-20 03:44:19 -0800

    update

--- test/NFTickets.js
+++ test/NFTickets.js
@@ -8,22 +8,19 @@ const Wallet = require('ethereumjs-wallet').default;
 const TokenMock = artifacts.require('TokenMock');
 const LimitOrderProtocol = artifacts.require('LimitOrderProtocol');
 const AggregatorMock = artifacts.require('AggregatorMock');
+const DatePriceCalculatorMock = artifacts.require('DatePriceCalculator');
 
 const { buildOrderData } = require('./helpers/orderUtils');
 const { toBN, cutLastArg } = require('./helpers/utils');
 
 describe('NFTickets', async function () {
-    let _, wallet, addr1;
+    let _, wallet;
     const privatekey = '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
     const account = Wallet.fromPrivateKey(Buffer.from(privatekey, 'hex'));
 
     function buildInverseWithSpread (inverse, spread) {
         return toBN(spread).setn(255, inverse).toString();
     }
-
-    // function buildSinglePriceGetter (swap, oracle, inverse, spread, amount = '0') {
-    //    return swap.contract.methods.singlePrice(oracle.address, buildInverseWithSpread(inverse, spread), amount).encodeABI();
-    // }
 
     // eslint-disable-next-line no-unused-vars
     function buildDoublePriceGetter (swap, oracle1, oracle2, spread, amount = '0') {
@@ -63,16 +60,17 @@ describe('NFTickets', async function () {
     }
 
     before(async function () {
-        [_, wallet, addr1] = await web3.eth.getAccounts();
+        [_, wallet] = await web3.eth.getAccounts();
     });
 
     beforeEach(async function () {
         this.dai = await TokenMock.new('DAI', 'DAI');
         this.weth = await TokenMock.new('WETH', 'WETH');
         this.inch = await TokenMock.new('1INCH', '1INCH');
-        this.NFTicket = await TokenMock.new('Morgenshtern', 'MORG');// await NFTMock.new("Morgenshtern", "MORG");
+        this.morg = await TokenMock.new('Morgenshtern', 'MORG');// await NFTMock.new("Morgenshtern", "MORG");
         
         this.swap = await LimitOrderProtocol.new();
+        this.datePriceCalculator = await DatePriceCalculatorMock.new();
 
         this.chainId = await this.dai.getChainId();
 
@@ -82,45 +80,44 @@ describe('NFTickets', async function () {
         await this.dai.mint(_, ether('1000000'));
         await this.weth.mint(_, ether('1000000'));
         await this.inch.mint(_, ether('1000000'));
-        await this.NFTicket.mint(wallet, 10);
-        // await this.NFTicket.safeMint(wallet, '1');
-        // await this.NFTicket.safeMint(wallet, '2');
-        // await this.NFTicket.safeMint(wallet, '3');
-        // await this.NFTicket.safeMint(wallet, '4');
-        // await this.NFTicket.safeMint(wallet, '5');
-        // await this.NFTicket.safeMint(wallet, '6');
-        // await this.NFTicket.safeMint(wallet, '7');
-        // await this.NFTicket.safeMint(wallet, '8');
-        // await this.NFTicket.safeMint(wallet, '9');
-        // await this.NFTicket.safeMint(wallet, '10');
-        const balance = await this.NFTicket.balanceOf(wallet);
+        await this.morg.mint(wallet, 10);
+        // await this.morg.safeMint(wallet, '1');
+        // await this.morg.safeMint(wallet, '2');
+        // await this.morg.safeMint(wallet, '3');
+        // await this.morg.safeMint(wallet, '4');
+        // await this.morg.safeMint(wallet, '5');
+        // await this.morg.safeMint(wallet, '6');
+        // await this.morg.safeMint(wallet, '7');
+        // await this.morg.safeMint(wallet, '8');
+        // await this.morg.safeMint(wallet, '9');
+        // await this.morg.safeMint(wallet, '10');
+        const balance = await this.morg.balanceOf(wallet);
         console.log('Issued ' + balance + ' NFT tickets');
 
         await this.dai.approve(this.swap.address, ether('1000000'));
         await this.weth.approve(this.swap.address, ether('1000000'));
         await this.inch.approve(this.swap.address, ether('1000000'));
-        await this.NFTicket.approve(this.swap.address, 10);
-        // await this.NFTicket.setApprovalForAll(this.swap.address, true);
+        await this.morg.approve(this.swap.address, 10);
+        // await this.morg.setApprovalForAll(this.swap.address, true);
         await this.dai.approve(this.swap.address, ether('1000000'), { from: wallet });
         await this.weth.approve(this.swap.address, ether('1000000'), { from: wallet });
         await this.inch.approve(this.swap.address, ether('1000000'), { from: wallet });
-        await this.NFTicket.approve(this.swap.address, 10, { from: wallet });
+        await this.morg.approve(this.swap.address, 10, { from: wallet });
 
         this.daiOracle = await AggregatorMock.new(ether('0.00025'));
     });
 
-    // Purchase of ticket ( Filled 2 orders)
-    xit('NFT sell', async function () {
-        const makerNFTicket = await this.NFTicket.balanceOf(wallet);
-        const takerNFTicket = await this.NFTicket.balanceOf(_);
+    // Purchase of 2 tickets ( Filled 2 orders)
+    it('NFT sell', async function () {
+        const makerMorg = await this.morg.balanceOf(wallet);
+        const takerMorg = await this.morg.balanceOf(_);
         const makerWeth = await this.weth.balanceOf(wallet);
         const takerWeth = await this.weth.balanceOf(_);
-        console.log('Balances: %s %s %s %s', makerNFTicket, takerNFTicket, makerWeth, takerWeth);
 
-        for (i = 0; i < 2; i++) {
+        for (let i = 0; i < 2; i++) {
             console.log('enter');
             const order = buildOrder(
-                i.toString(), this.NFTicket, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x',
+                i.toString(), this.morg, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x',
             );
             const data = buildOrderData(this.chainId, this.swap.address, order);
             const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
@@ -128,45 +125,40 @@ describe('NFTickets', async function () {
             await this.swap.fillOrder(order, signature, 0, ether('1000'), 1);
             console.log('filled order');
         }
-        const makerNFTicket1 = await this.NFTicket.balanceOf(wallet);
-        const takerNFTicket1 = await this.NFTicket.balanceOf(_);
-        const makerWeth1 = await this.weth.balanceOf(wallet);
-        const takerWeth1 = await this.weth.balanceOf(_);
-        console.log('Balances: %d %d %d %d', makerNFTicket1, takerNFTicket1, makerWeth1 / 10 ** 18, takerWeth1 / 10 ** 18);
 
-        expect(await this.NFTicket.balanceOf(wallet)).to.be.bignumber.equal(makerNFTicket.sub(web3.utils.toBN('2')));
-        expect(await this.NFTicket.balanceOf(_)).to.be.bignumber.equal(takerNFTicket.add(web3.utils.toBN('2')));
+        expect(await this.morg.balanceOf(wallet)).to.be.bignumber.equal(makerMorg.sub(web3.utils.toBN('2')));
+        expect(await this.morg.balanceOf(_)).to.be.bignumber.equal(takerMorg.add(web3.utils.toBN('2')));
         expect(await this.weth.balanceOf(wallet)).to.be.bignumber.equal(makerWeth.add(ether('2000')));
         expect(await this.weth.balanceOf(_)).to.be.bignumber.equal(takerWeth.sub(ether('2000')));
     });
 
     // Ticket made for specific buyer ( allowedSender )
     describe('VIP ticket', async function () {
-        xit('should fill with correct taker', async function () {
+        it('should fill with correct taker', async function () {
             const order = buildOrder(
-                '1', this.NFTicket, this.weth, '1'.toString(), '1'.toString(), '0x', '0x',
+                '1', this.morg, this.weth, '1'.toString(), '1'.toString(), '0x', '0x',
             );
             order.allowedSender = _;
             const data = buildOrderData(this.chainId, this.swap.address, order);
             const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
 
-            const makerNFTicket = await this.NFTicket.balanceOf(wallet);
-            const takerNFTicket = await this.NFTicket.balanceOf(_);
+            const makerMorg = await this.morg.balanceOf(wallet);
+            const takerMorg = await this.morg.balanceOf(_);
             const makerWeth = await this.weth.balanceOf(wallet);
             const takerWeth = await this.weth.balanceOf(_);
-            console.log('Balances: %s %s %s %s', makerNFTicket, takerNFTicket, makerWeth, takerWeth);
+            console.log('Balances: %s %s %s %s', makerMorg, takerMorg, makerWeth, takerWeth);
 
             await this.swap.fillOrder(order, signature, 0, 1, 1);
 
-            expect(await this.NFTicket.balanceOf(wallet)).to.be.bignumber.equal(makerNFTicket.sub(web3.utils.toBN('1')));
-            expect(await this.NFTicket.balanceOf(_)).to.be.bignumber.equal(takerNFTicket.add(web3.utils.toBN('1')));
+            expect(await this.morg.balanceOf(wallet)).to.be.bignumber.equal(makerMorg.sub(web3.utils.toBN('1')));
+            expect(await this.morg.balanceOf(_)).to.be.bignumber.equal(takerMorg.add(web3.utils.toBN('1')));
             expect(await this.weth.balanceOf(wallet)).to.be.bignumber.equal(makerWeth.add(web3.utils.toBN('1')));
             expect(await this.weth.balanceOf(_)).to.be.bignumber.equal(takerWeth.sub(web3.utils.toBN('1')));
         });
 
         it('should not fill with incorrect taker', async function () {
             const order = buildOrder(
-                '1', this.NFTicket, this.weth, '1'.toString(), '1'.toString(), '0x', '0x',
+                '1', this.morg, this.weth, '1'.toString(), '1'.toString(), '0x', '0x',
             );
             order.allowedSender = wallet;
             const data = buildOrderData(this.chainId, this.swap.address, order);
@@ -180,73 +172,58 @@ describe('NFTickets', async function () {
     });
 
     // Variable price depending on the market conditions ( getTakerAmount )
-    xit('Ticket price protection', async function () {
+    it('Ticket price protection', async function () {
         const makerAmount = 1;
         const takerAmount = ether('1');
         const order = buildOrder(
-            '1', this.NFTicket, this.weth, '1'.toString(), ether('1').toString(), '0x',
+            '1', this.morg, this.weth, '1'.toString(), ether('1').toString(), '0x',
             cutLastArg(this.swap.contract.methods.getTakerAmount(makerAmount, takerAmount, 0).encodeABI()),
         );
 
         const data = buildOrderData(this.chainId, this.swap.address, order);
         const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
 
-        const makerNFTicket = await this.NFTicket.balanceOf(wallet);
-        const takerNFTicket = await this.NFTicket.balanceOf(_);
+        const makerMorg = await this.morg.balanceOf(wallet);
+        const takerMorg = await this.morg.balanceOf(_);
         const makerWeth = await this.weth.balanceOf(wallet);
         const takerWeth = await this.weth.balanceOf(_);
 
         await this.swap.fillOrder(order, signature, makerAmount, 0, takerAmount.add(ether('0.01')));
-        console.log('filled order');
 
-        const makerNFTicket1 = await this.NFTicket.balanceOf(wallet);
-        const takerNFTicket1 = await this.NFTicket.balanceOf(_);
-        const makerWeth1 = await this.weth.balanceOf(wallet);
-        const takerWeth1 = await this.weth.balanceOf(_);
-        console.log('Balances: %d %d %d %d', makerNFTicket1, takerNFTicket1, makerWeth1, takerWeth1);
-
-        expect(await this.NFTicket.balanceOf(wallet)).to.be.bignumber.equal(makerNFTicket.sub(web3.utils.toBN('1')));
-        expect(await this.NFTicket.balanceOf(_)).to.be.bignumber.equal(takerNFTicket.add(web3.utils.toBN('1')));
+        expect(await this.morg.balanceOf(wallet)).to.be.bignumber.equal(makerMorg.sub(web3.utils.toBN('1')));
+        expect(await this.morg.balanceOf(_)).to.be.bignumber.equal(takerMorg.add(web3.utils.toBN('1')));
         expect(await this.weth.balanceOf(wallet)).to.be.bignumber.equal(makerWeth.add(ether(makerAmount.toString())));
         expect(await this.weth.balanceOf(_)).to.be.bignumber.equal(takerWeth.sub(ether(makerAmount.toString())));
     });
 
     // Sale expiration ( Predicate )
     describe('Ticket sale finish', async function () {
-        xit('should sell when not finished', async function () {
-
+        it('should sell when not finished', async function () {
             const order = buildOrder(
-                '1', this.NFTicket, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x', constants.ZERO_ADDRESS, 
+                '1', this.morg, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x', constants.ZERO_ADDRESS,
                 this.swap.contract.methods.timestampBelow(0xff00000000).encodeABI(),
             );
     
             const data = buildOrderData(this.chainId, this.swap.address, order);
             const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
     
-            const makerNFTicket = await this.NFTicket.balanceOf(wallet);
-            const takerNFTicket = await this.NFTicket.balanceOf(_);
+            const makerMorg = await this.morg.balanceOf(wallet);
+            const takerMorg = await this.morg.balanceOf(_);
             const makerWeth = await this.weth.balanceOf(wallet);
             const takerWeth = await this.weth.balanceOf(_);
-            console.log('Balances: %s %s %s %s', makerNFTicket, takerNFTicket, makerWeth, takerWeth);
     
             await this.swap.fillOrder(order, signature, 0, ether('1000'), 1);
-            console.log("fill order");
-            const makerNFTicket1 = await this.NFTicket.balanceOf(wallet);
-            const takerNFTicket1 = await this.NFTicket.balanceOf(_);
-            const makerWeth1 = await this.weth.balanceOf(wallet);
-            const takerWeth1 = await this.weth.balanceOf(_);
-            console.log('Balances: %d %d %d %d', makerNFTicket1, takerNFTicket1, makerWeth1 / 10 ** 18, takerWeth1 / 10 ** 18);
-    
-            expect(await this.NFTicket.balanceOf(wallet)).to.be.bignumber.equal(makerNFTicket.sub(web3.utils.toBN('1')));
-            expect(await this.NFTicket.balanceOf(_)).to.be.bignumber.equal(takerNFTicket.add(web3.utils.toBN('1')));
+
+            expect(await this.morg.balanceOf(wallet)).to.be.bignumber.equal(makerMorg.sub(web3.utils.toBN('1')));
+            expect(await this.morg.balanceOf(_)).to.be.bignumber.equal(takerMorg.add(web3.utils.toBN('1')));
             expect(await this.weth.balanceOf(wallet)).to.be.bignumber.equal(makerWeth.add(ether('1000')));
             expect(await this.weth.balanceOf(_)).to.be.bignumber.equal(takerWeth.sub(ether('1000')));
         });
 
-        xit('should not sell when finished', async function () {
+        it('should not sell when finished', async function () {
             const order = buildOrder(
-                '1', this.NFTicket, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x', constants.ZERO_ADDRESS, 
-                this.swap.contract.methods.timestampBelow(0xff0000).encodeABI()
+                '1', this.morg, this.weth, '1'.toString(), ether('1000').toString(), '0x', '0x', constants.ZERO_ADDRESS,
+                this.swap.contract.methods.timestampBelow(0xff0000).encodeABI(),
             );
             const data = buildOrderData(this.chainId, this.swap.address, order);
             const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
@@ -254,6 +231,53 @@ describe('NFTickets', async function () {
             await expectRevert(
                 this.swap.fillOrder(order, signature, 0, ether('1000'), 1),
                 'LOP: predicate returned false',
+            );
+        });
+    });
+
+    // Price change based on date ( getTakerAmount )
+    describe('Ticket sale increase', async function () {
+        it('Second butch for x2', async function () {
+            const order = buildOrder(
+                '1', this.morg, this.weth, '1'.toString(), ether('1000').toString(), '0x',
+                cutLastArg(
+                    this.swap.contract.methods.arbitraryStaticCall(
+                        this.datePriceCalculator.address,
+                        cutLastArg(this.datePriceCalculator.contract.methods.singlePrice(0xff0, ether('1000')).encodeABI(), -64))
+                        .encodeABI(), -64),
+            );
+
+            const data = buildOrderData(this.chainId, this.swap.address, order);
+            const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
+    
+            const makerMorg = await this.morg.balanceOf(wallet);
+            const takerMorg = await this.morg.balanceOf(_);
+            const makerWeth = await this.weth.balanceOf(wallet);
+            const takerWeth = await this.weth.balanceOf(_);
+    
+            await this.swap.fillOrder(order, signature, 1, 0, ether('2000.01'));
+    
+            expect(await this.morg.balanceOf(wallet)).to.be.bignumber.equal(makerMorg.sub(web3.utils.toBN('1')));
+            expect(await this.morg.balanceOf(_)).to.be.bignumber.equal(takerMorg.add(web3.utils.toBN('1')));
+            expect(await this.weth.balanceOf(wallet)).to.be.bignumber.equal(makerWeth.add(ether('2000')));
+            expect(await this.weth.balanceOf(_)).to.be.bignumber.equal(takerWeth.sub(ether('2000')));
+        });
+
+        it('second batch doesn\'t sell for initial price', async function () {
+            const order = buildOrder(
+                '1', this.morg, this.weth, '1'.toString(), ether('1000').toString(), '0x',
+                cutLastArg(
+                    this.swap.contract.methods.arbitraryStaticCall(
+                        this.datePriceCalculator.address,
+                        cutLastArg(this.datePriceCalculator.contract.methods.singlePrice(0xff0, ether('1000')).encodeABI(), -64))
+                        .encodeABI(), -64),
+            );
+            const data = buildOrderData(this.chainId, this.swap.address, order);
+            const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
+
+            await expectRevert(
+                this.swap.fillOrder(order, signature, 1, 0, ether('1000.01')),
+                'LOP: taking amount too high',
             );
         });
     });
@@ -285,27 +309,5 @@ describe('NFTickets', async function () {
         expect(await this.dai.balanceOf(_)).to.be.bignumber.equal(takerDai.sub(takerAmount));
         expect(await this.inch.balanceOf(wallet)).to.be.bignumber.equal(makerInch.sub(makerAmount));
         expect(await this.inch.balanceOf(_)).to.be.bignumber.equal(takerInch.add(makerAmount));
-    });
-
-    xit('dai -> 1inch stop loss order predicate is invalid', async function () {
-        const makerAmount = ether('100');
-        const takerAmount = ether('631');
-        const priceCall = buildDoublePriceGetter(this.swap, this.inchOracle, this.daiOracle, '1000000000', ether('1'));
-        const predicate = this.swap.contract.methods.lt(ether('6.31'), this.swap.address, priceCall).encodeABI();
-
-        const order = buildOrder(
-            '1', this.inch, this.dai, makerAmount.toString(), takerAmount.toString(),
-            cutLastArg(this.swap.contract.methods.getMakerAmount(makerAmount, takerAmount, 0).encodeABI()),
-            cutLastArg(this.swap.contract.methods.getTakerAmount(makerAmount, takerAmount, 0).encodeABI()),
-            constants.ZERO_ADDRESS,
-            predicate,
-        );
-        const data = buildOrderData(this.chainId, this.swap.address, order);
-        const signature = ethSigUtil.signTypedMessage(account.getPrivateKey(), { data });
-
-        await expectRevert(
-            this.swap.fillOrder(order, signature, makerAmount, 0, takerAmount.add(ether('0.01'))), // taking threshold = exact taker amount + eps
-            'LOP: predicate returned false',
-        );
     });
 });
